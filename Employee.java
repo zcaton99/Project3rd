e9f4881a5c181fb50ea5c09ebafb1aa5eca66167
Josephpbermingham <31869040+Josephpbermingham@users.noreply.github.com>
2017-10-27 16:30:32 -0400

    Add files via upload
    This is the new Employee With The Getters and setters

--- Employee.java
+++ Employee.java
@@ -1,24 +1,50 @@
 package sample;
 
 public class Employee {
-
     private String firstName;
     private String lastName;
     private String username;
     private String password;
     private String email;
-    public Employee(String fName,String lName,String uName,String Pword,String Email){
-        this.firstName=fName;
-        this.lastName=lName;
-        this.username=uName;
-        this.password=Pword;
-        this.email=Email;
+
+
+    public String getFirstName() {
+        return firstName;
     }
-    public Employee(){
-        this.firstName="John";
-        this.lastName="doe";
-        this.username="SysAdmin";
-        this.password="A";
-        this.email="J@J.com";
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword(boolean a) {
+        if (a)
+            return password;
+        else
+            return "";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public Employee(String fName, String lName, String uName, String Pword, String Email) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.username = uName;
+        this.password = Pword;
+        this.email = Email;
+    }
+
+    public Employee() {
+        this.firstName = "John";
+        this.lastName = "doe";
+        this.username = "SysAdmin";
+        this.password = "A";
+        this.email = "J@J.com";
     }
 }
